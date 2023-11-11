acfb5fe132fdc798325e65bc6d8a353c495337bb
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2023-11-11 19:22:42 +0900

    feat: show hasModuleSyntax

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,6 +1,7 @@
 import { useMemo } from 'preact/hooks'
 import { ResultCode } from './ResultCode'
 import { usePackage } from './usePackage'
+import { isVersionLower } from './parseVersion'
 
 export const Result = ({
   version,
@@ -20,19 +21,39 @@ export const Result = ({
       return { type: 'error', value: e } as const
     }
   }, [esModuleLexer, input])
+  const hasModuleSyntax = useMemo(() => {
+    if (result.type !== 'success') return undefined
+    if (isVersionLower(version, '1.4.0')) return 'Not supported in this version'
+    return JSON.stringify(result.value[3])
+  }, [result, version])
 
   return (
     <div class="result">
       {result.type === 'loading' ? 'loading...' : null}
       {result.type === 'success' ? (
         <>
-          <div>facade: {JSON.stringify(result.value[2])}</div>
+          <div class="result-meta">
+            <p>
+              <span class="result-meta-title">facade</span>:{' '}
+              {JSON.stringify(result.value[2])}
+            </p>
+            <p>
+              <span class="result-meta-title">hasModuleSyntax</span>:{' '}
+              {hasModuleSyntax}
+            </p>
+          </div>
           <div class="panel">
-            <ResultCode source={input} result={result.value} version={version} />
+            <ResultCode
+              source={input}
+              result={result.value}
+              version={version}
+            />
           </div>
         </>
       ) : null}
-      {result.type === 'error' ? <div class="result-error">{`${result.value}`}</div> : null}
+      {result.type === 'error' ? (
+        <div class="result-error">{`${result.value}`}</div>
+      ) : null}
     </div>
   )
 }
